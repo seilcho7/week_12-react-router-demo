8f4b4ed04376fcb8eb24620bd11b875f9b73a6ed
Seil Cho <seilcho7@hotmail.com>
2019-04-30 15:22:17 -0400

    add links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Home from './Home';
+import About from './About';
+import Cats from './Cats';
+
+import {
+  Link,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Link is what you use instead of an anchor! */}
+      <Link to="/">Home</Link>
+      <Link to="/cats">Cats</Link>
+      <Link to="/about">About</Link>
+      {/* <Home /> */}
+      <Route exact path="/" component={Home} />
+      {/* <About /> */}
+      <Route path="/cats" component={Cats} />
+      {/* <Cats /> */}
+      <Route path="/about" component={About} />
     </div>
   );
 }
---
+++ src/Cats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Cats(props) {
+    return (
+        <div>
+            <h1>🐱</h1>
+        </div>
+    )
+}
+
+export default Cats;
